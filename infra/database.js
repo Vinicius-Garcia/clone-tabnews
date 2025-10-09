6f52caa6401e5ee397473738f3ff089ab72becf5
Vinicius Garcia <viniciusgarcia1300@gmail.com>
2025-10-09 14:22:37 -0300

    Adicionado base do database

---
+++ infra/database.js
@@ -0,0 +1,19 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
+import database from "../../../../infra/database.js";
+
 function status(req, res) {
+  const result = database.query("SELECT 1");
+  console.log(result);
   res.status(200).json({ status: "ok" });
 }
 
