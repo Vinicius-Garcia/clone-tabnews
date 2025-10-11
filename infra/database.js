eb8ada5b7de37176a6cce00c6f15c22e837e017d
Vinicius Garcia <viniciusgarcia1300@gmail.com>
2025-10-10 22:57:18 -0300

    add try catch in db connection

--- infra/database.js
+++ infra/database.js
@@ -9,9 +9,15 @@ async function query(queryObject) {
     password: process.env.DB_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log("Database query error:");
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
